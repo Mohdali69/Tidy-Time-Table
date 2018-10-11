d7ff44e45b0a8199c0ae7dbcb93a30c02da5ee49
Mohdali69 <ali6998@protonmail.com>
2018-10-11 12:44:15 +0200

    Ajouts des Fragment Taches Routines Profil Envoyer Partager_Frag.java

--- app/src/main/java/com/example/mohdali/tidytimetable/Premiere_Page.java
+++ app/src/main/java/com/example/mohdali/tidytimetable/Premiere_Page.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class Premiere_Page extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -80,17 +82,31 @@ public class Premiere_Page extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_ajouter_taches) {
+            FragmentManager fm = getSupportFragmentManager();
+            fm.beginTransaction().replace(R.id.main,new Ajouter_Taches_Frag()).commit();
+            Toast.makeText(this,"Ajouter des taches test",Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_taches) {
+            FragmentManager fm = getSupportFragmentManager();
+            fm.beginTransaction().replace(R.id.main,new Mes_Taches_Frag()).commit();
+            Toast.makeText(this,"Mes Taches test",Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_routines) {
+            FragmentManager fm = getSupportFragmentManager();
+            fm.beginTransaction().replace(R.id.main,new Routines_Frag()).commit();
+            Toast.makeText(this,"Mettre des Routines test",Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_profil) {
+            FragmentManager fm = getSupportFragmentManager();
+            fm.beginTransaction().replace(R.id.main,new Profil_Frag()).commit();
+            Toast.makeText(this,"Regarder vos Profil test",Toast.LENGTH_LONG).show();
+        } else if (id == R.id.nav_partager) {
+            FragmentManager fm = getSupportFragmentManager();
+            fm.beginTransaction().replace(R.id.main,new Partager_Frag()).commit();
+            Toast.makeText(this,"Partager des taches test",Toast.LENGTH_LONG).show();
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_envoyer) {
+            FragmentManager fm = getSupportFragmentManager();
+            fm.beginTransaction().replace(R.id.main,new Envoyer_Frag()).commit();
+            Toast.makeText(this,"Envoyer des taches test",Toast.LENGTH_LONG).show();
 
         }
 
