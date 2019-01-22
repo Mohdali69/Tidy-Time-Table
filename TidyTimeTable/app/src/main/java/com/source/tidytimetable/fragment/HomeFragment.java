3c036853384ce452160233e383934dd2bc50cc98
Mohdali69 <ali6998@protonmail.com>
2019-01-22 23:53:28 +0100

    Regler probleme BD et Ajout de l'apk et deployement avec Issam

--- TidyTimeTable/app/src/main/java/com/source/tidytimetable/fragment/HomeFragment.java
+++ TidyTimeTable/app/src/main/java/com/source/tidytimetable/fragment/HomeFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.source.tidytimetable.*;
 
@@ -14,6 +15,8 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        LinearLayout v = (LinearLayout) inflater.inflate(R.layout.fragment_home, container, false);
+
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
